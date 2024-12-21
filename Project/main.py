ff143dc9c50e1ca08594bab7b02cbbb97018a20a
Lavder12 <vladgabelko03@gmail.com>
2024-12-21 15:58:38 +0100

    v.0.1.4

--- Project/main.py
+++ Project/main.py
@@ -1,10 +1,17 @@
 from db_manager import DBManager
 from UserHand import User
 
-dbconfig = {'host': 'ich-db.ccegls0svc9m.eu-central-1.rds.amazonaws.com',
-            'user': 'ich1',
-            'password': 'password',
+# dbconfig = {'host': 'ich-db.ccegls0svc9m.eu-central-1.rds.amazonaws.com',
+#             'user': 'ich1',
+#             'password': 'password',
+#             'database': 'sakila'}
+
+dbconfig = {'host': 'localhost',
+            'port': '3306',
+            'user': 'root',
+            'password': 'root',
             'database': 'sakila'}
+
 
 if __name__ == '__main__':
     db_manager = DBManager(dbconfig)
